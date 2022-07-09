2bfd91ce471c5d900dcab62db3e8b297315b8e8a
JumHorn <JumHorn@gmail.com>
2022-07-09 20:45:48 +0800

    add Date Time

---
+++ DateTime.js
@@ -0,0 +1,7 @@
+var birthday = new Date('December 17, 1995 03:24:00')
+var today = new Date();
+today.toString();
+today.getDate();
+today.getDay();
+today.setDate();
+today.setHours();
---
+++ template.js
@@ -0,0 +1,8 @@
+// const variables
+
+const MOD = 1e9 + 7
+
+// board dfs direction
+const path = [-1, 0, 1, 0, -1]
+
+// frequently used functions
