6ed308488c13bbfa9fbcad4a6c4caae2bf30098b
JumHorn <JumHorn@gmail.com>
2022-07-09 20:33:04 +0800

    add data structure

---
+++ DataStructure.js
@@ -0,0 +1,10 @@
+
+var length = 7;//数字
+var lastName = "Gates";//字符串
+var cars = ["Porsche", "Volvo", "BMW"];//数组
+var x = { firstName: "Bill", lastName: "Gates" };//对象
+
+// set
+var set = new Set(["sumit", "sumit", "amit", "anil", "anish"]);
+set.add("amit");
+set.has("amit");
