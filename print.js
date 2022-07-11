c32bbd605476be7b504560edb31d796c40daa7c8
JumHorn <JumHorn@gmail.com>
2022-07-11 14:23:12 +0800

    print format

---
+++ print.js
@@ -0,0 +1,10 @@
+/*
+%s – Format as a string.
+%i – Format as an integer.
+%f – Format as a floating point value.
+%O – Format as a JavaScript object.
+%o – Format as a DOM element.
+%c – Format as a CSS rule, which is applied to the emitted log line.
+*/
+
+console.log("%d,%f", 8, 8.1)
