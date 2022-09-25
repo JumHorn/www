71bb0d74a0ffa5afd0cd8cb3dcb0da587e615e4a
JumHorn <JumHorn@gmail.com>
2022-09-25 17:07:42 +0800

    json

---
+++ json.js
@@ -0,0 +1,22 @@
+//
+
+text = `{
+  "browsers": {
+    "firefox": {
+      "name": "Firefox",
+      "pref_url": "about:config",
+      "releases": {
+        "1": {
+          "release_date": "2004-11-09",
+          "status": "retired",
+          "engine": "Gecko",
+          "engine_version": "1.7"
+        }
+      }
+    }
+  }
+}`
+
+jobj = JSON.parse(text)
+
+jobj = jobj["browsers"]
