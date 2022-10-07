b51308cdd543586fc925b1b3c45ade6931d02586
JumHorn <JumHorn@gmail.com>
2022-10-07 19:44:27 +0800

    closures

--- sleep.js
+++ sleep.js
@@ -1,5 +1,7 @@
+// timeout
 setTimeout(() => { console.log("hello world"); }, 1000)
 
+// origin version
 function sleep(time) {
 	return new Promise((resolve) => setTimeout(resolve, time));
 }
@@ -14,3 +16,23 @@ function doTask() {
 }
 
 sleep(5000).then(doTask);
+
+// avoid global varible
+function sleep1(time) {
+	let i = 0;
+	function doTask() {
+		++i;
+		if (i < 10) {
+			console.log("sleep " + i);
+			sleep(time).then(doTask);
+		}
+	}
+
+	function sleep(time) {
+		return new Promise((resolve) => setTimeout(resolve, time));
+	}
+
+	sleep(time).then(doTask);
+}
+
+// 闭包(Closures)
