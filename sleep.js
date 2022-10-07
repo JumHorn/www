7752bae7a20be117c2a748c56e7ffc4ccf273cd4
JumHorn <JumHorn@gmail.com>
2022-10-07 19:33:58 +0800

    real sleep

--- sleep.js
+++ sleep.js
@@ -1,1 +1,16 @@
 setTimeout(() => { console.log("hello world"); }, 1000)
+
+function sleep(time) {
+	return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+let i = 0;
+function doTask() {
+	++i;
+	if (i < 10) {
+		console.log("sleep " + i);
+		sleep(5000).then(doTask);
+	}
+}
+
+sleep(5000).then(doTask);
