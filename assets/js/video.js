0a7cb3b851f9d16fa14cf6520169785dc4f81d35
JumHorn <JumHorn@gmail.com>
2025-01-04 14:11:42 +0000

    dynamic urls

--- assets/js/video.js
+++ assets/js/video.js
@@ -15,3 +15,26 @@ function onPlay() {
 		play.src = streamurl + vipurl;
 	}
 }
+
+async function onLoad() {
+	const response = await fetch('/api/vipurl');
+	const vipurl = await response.json();
+	if (!vipurl || vipurl.length === 0)
+		return;
+	var selecturl = document.getElementById("stream");
+	selecturl.replaceChildren(); // 删除所有子节点
+	let index = 0;
+	for (const url of vipurl) {
+		++index;
+		const opt = document.createElement("option");
+		opt.value = url;
+		opt.textContent = "线路" + index;
+		if (index === 1) {
+			opt.setAttribute("selected", "");
+			opt.selected = true;
+		}
+		selecturl.appendChild(opt)
+	}
+}
+
+window.onload = onLoad();
